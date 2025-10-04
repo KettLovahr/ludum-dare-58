6ced94e30247ef3c6faf7e8793ec20448f4597c9
Kett Lovahr <catusfelony@gmail.com>
2025-10-04 19:56:36 -0300

    Aura

--- Scenes/Objects/Player/PlayerPart.gd
+++ Scenes/Objects/Player/PlayerPart.gd
@@ -2,6 +2,10 @@
 extends CharacterBody2D
 class_name PlayerPart
 
+@export var sprites: Array[Sprite2D]
+@onready var glow_material: Material
+
+
 const GRAVITY: float = 1000
 
 var selected: bool:
@@ -11,6 +15,9 @@ var selected: bool:
 			return parent.selected == self
 		return false
 
+
+func _process(delta) -> void:
+	glow_material.set_shader_parameter("glowing", self.selected)
 
 func _physics_process(delta: float) -> void:
 	if selected and controllable:
@@ -24,6 +31,8 @@ func _handle_controls(delta: float): pass
 func _custom_behavior(delta: float): pass
 
 func _ready():
+	# this is stupid but i want the state for these to be different always :/
+	glow_material = load("res://Scenes/Objects/Player/Parts/PartGlow.tres").duplicate_deep(Resource.DEEP_DUPLICATE_ALL)
 	for child in get_children():
 		if child is PartSelectionArea:
 			child.part_clicked.connect(func(): select())
@@ -32,5 +41,7 @@ func select():
 	var parent = get_parent()
 	if parent is PlayerRoot:
 		parent.request_selection(self)
+	for sprite in sprites:
+		sprite.material = glow_material
 
 @export var controllable: bool = true
