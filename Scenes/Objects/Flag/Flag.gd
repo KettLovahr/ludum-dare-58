43e38c5cc4f43b4db2c686744607582007c9280e
Kett Lovahr <catusfelony@gmail.com>
2025-10-04 20:58:17 -0300

    Implement coin and flags

---
+++ Scenes/Objects/Flag/Flag.gd
@@ -0,0 +1,25 @@
+extends Area2D
+class_name Flag
+
+signal flag_reached
+
+var parts_reached: Array[PlayerPart]
+@export var player_root: PlayerRoot
+var target_count: int
+
+func _ready():
+	target_count = len(player_root.get_children().filter(func(x): return x is PlayerPart))
+
+func _on_body_entered(body: Node2D) -> void:
+	if body is PlayerPart and body not in parts_reached:
+		parts_reached.append(body)
+	if len(parts_reached) == target_count:
+		flag_reached.emit()
+	print(parts_reached)
+		
+func _on_body_exited(body: Node2D) -> void:
+	for i in range(len(parts_reached)):
+		if parts_reached[i] == body:
+			parts_reached.remove_at(i)
+			break
+	print(parts_reached)
