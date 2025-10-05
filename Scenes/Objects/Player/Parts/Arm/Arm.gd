fccfa60e7674b9fbc30dc5c85a2edab8f5e702b2
Kett Lovahr <catusfelony@gmail.com>
2025-10-05 00:08:58 -0300

    Godot does not know how to convert an int into a float

--- Scenes/Objects/Player/Parts/Arm/Arm.gd
+++ Scenes/Objects/Player/Parts/Arm/Arm.gd
@@ -4,4 +4,4 @@ class_name Arm
 func _custom_behavior(delta: float):
 	velocity.y += GRAVITY * delta
 	if is_on_floor():
-		velocity.x = lerp(velocity.x, 0, 0.2)
+		velocity.x = lerp(velocity.x, 0.0, 0.2)
