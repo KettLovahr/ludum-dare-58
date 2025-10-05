3609197e869816c5cba2b4d370f79e6a1b3ef578
Kett Lovahr <catusfelony@gmail.com>
2025-10-05 15:23:44 -0300

    Leg can only kick forward

--- Scenes/Objects/Player/Parts/Leg/KickArea.gd
+++ Scenes/Objects/Player/Parts/Leg/KickArea.gd
@@ -7,6 +7,8 @@ var curve: Curve2D = Curve2D.new()
 var dist: float
 var height: float
 
+var valid_kick: bool = false
+
 func _ready():
 	generate_curve(1, 1.0/60)
 	%TrajectoryArc.curve = curve
@@ -14,10 +16,17 @@ func _ready():
 func _process(delta: float) -> void:
 	if kick_target != null:
 		var strength = kick_target.global_position.x - global_position.x
-		generate_curve(strength, delta)
+		valid_kick = generate_curve(strength, delta)
 		%TrajectoryArc.global_position = kick_target.global_position
+	%TrajectoryArc.visible = (kick_target != null)\
+		and get_parent().can_kick\
+		and valid_kick
 		
-func generate_curve(strength: float, delta: float):
+func generate_curve(strength: float, delta: float) -> bool:
+	var parent: Leg = get_parent()
+	if parent.facing == Leg.Direction.LEFT and strength > 0\
+		or parent.facing == Leg.Direction.RIGHT and strength < 0:
+			return false
 	dist = strength * 6
 	height = abs(strength) * 6
 	if kick_target:
@@ -32,18 +41,16 @@ func generate_curve(strength: float, delta: float):
 		curve.add_point(Vector2(dist * i * delta, yp))
 		yp += yvel * delta
 		yvel += PlayerPart.GRAVITY * delta
+	return true
 	
 func handle_kick():
 	kick_target.velocity.x = dist
 	kick_target.velocity.y = -height
-	%TrajectoryArc.hide()
 
 func _on_body_entered(body: Node2D) -> void:
 	if body is PlayerPart and body != get_parent() and kick_target == null:
 		kick_target = body
-		%TrajectoryArc.show()
 
 func _on_body_exited(body: Node2D) -> void:
 	if body == kick_target:
 		kick_target = null
-		%TrajectoryArc.hide()
