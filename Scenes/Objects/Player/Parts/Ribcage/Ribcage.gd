85b855f28f06d517b2e0fbbba914fd23e5d51b55
Fupicat <36973337+fupicat@users.noreply.github.com>
2025-10-06 00:31:35 -0300

    ribcage animado e novo nível!

--- Scenes/Objects/Player/Parts/Ribcage/Ribcage.gd
+++ Scenes/Objects/Player/Parts/Ribcage/Ribcage.gd
@@ -1,7 +1,7 @@
 extends PlayerPart
 class_name Ribcage
 
-const MOVE_SPEED = 600
+const MOVE_SPEED = 400
 const CLIMB_SPEED = 200
 
 var climbing: bool = false
@@ -10,26 +10,54 @@ var carrying: PlayerPart
 var climbable_areas: Array[ClimbableArea]
 var detected_parts: Array[PlayerPart]
 
+@onready var root = $Root
+@onready var anim = $Root/AnimationPlayer
+
 func _custom_behavior(delta: float):
 	if not climbing:
 		velocity.y += GRAVITY * delta
 	if carrying:
 		carrying.global_position = $CarryPivot.global_position
+		carrying.velocity = Vector2.ZERO
+	if not selected:
+		if climbing:
+			anim.play("climb_idle")
+		else:
+			anim.play("side_idle")
 
 func _handle_controls(delta: float):
-	velocity.x = Input.get_axis("move_left", "move_right") *\
-		(CLIMB_SPEED if climbing else MOVE_SPEED)
+	var x_axis = Input.get_axis("move_left", "move_right")
+	var y_axis = Input.get_axis("move_up", "move_down")
+	velocity.x = x_axis * (CLIMB_SPEED if climbing else MOVE_SPEED)
 	if climbing:
-		velocity.y = Input.get_axis("move_up", "move_down") * CLIMB_SPEED
+		velocity.y = y_axis * CLIMB_SPEED
 	if not climbing and Input.is_action_pressed("move_up") and can_climb():
 		climbing = true
 	if not climbing and not carrying and len(detected_parts) > 0:
 		if Input.is_action_just_pressed("move_action"):
 			carrying = detected_parts[0]
+			carrying.z_index = 1
 	elif carrying:
 		if Input.is_action_just_pressed("move_action"):
+			carrying.z_index = 0
 			carrying = null
-		
+
+	if climbing:
+		root.scale.x = 1
+		if x_axis != 0 or y_axis != 0:
+			anim.play("climb_walk")
+		else:
+			anim.play("climb_idle")
+	else:
+		if x_axis > 0:
+			root.scale.x = 1
+			anim.play("side_walk")
+		elif x_axis < 0:
+			root.scale.x = -1
+			anim.play("side_walk")
+		else:
+			anim.play("side_idle")
+
 func can_climb() -> bool:
 	return len(climbable_areas) > 0
 
@@ -48,4 +76,3 @@ func _on_pickup_area_body_exited(body: Node2D) -> void:
 			if detected_parts[i] == body:
 				detected_parts.remove_at(i)
 				break
-		
